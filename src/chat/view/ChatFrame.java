f994f9d11d2775f754ccf39692d9a6b885f2b71b
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-02-06 09:01:21 -0700

    Added panel

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,15 +6,24 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private Controller app;
+	private ChatPanel panel;
 	
 	public ChatFrame(Controller app)
 	{
 		super();
 		this.app = app;
+		this.panel = new ChatPanel(this.app);
+		
+		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		
+		setContentPane(panel);
+		setSize(1027, 768);
+		setTitle("Chatbot");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setVisible(true);
 	}
 }
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,34 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.Controller;
+
+public class ChatPanel extends JPanel
+{
+	private Controller app;
+	
+	public ChatPanel(Controller app)
+	{
+		super();
+		this.app  = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+}
