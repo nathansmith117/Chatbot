35bd1b4d81edebf93ef662dd1eb046b44a2137d3
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-02-06 08:45:15 -0700

    Create the frame

---
+++ src/chat/view/ChatFrame.java
@@ -0,0 +1,20 @@
+package chat.view;
+
+import chat.controller.Controller;
+import javax.swing.JFrame;
+
+public class ChatFrame extends JFrame
+{
+	private Controller app;
+	
+	public ChatFrame(Controller app)
+	{
+		super();
+		this.app = app;
+	}
+	
+	private void setupFrame()
+	{
+		
+	}
+}
